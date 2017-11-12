e3bb9f8e0971d7012123cad0cc6ba36d28462248
Alexander Pitzer <pitzera@gmx.de>
2017-11-12 17:38:04 +0100

    remove the explicit floatx cast (reverted from commit 35c39dcbeb5e27cdf7b795d6fe3ca188090910e6)

--- tools/image.py
+++ tools/image.py
@@ -1004,8 +1004,7 @@ class NumpyArrayIterator(Iterator):
 
         if data_format is None:
             data_format = K.image_data_format()
-        #self.x = np.asarray(x, dtype=K.floatx())
-        self.x = x
+        self.x = np.asarray(x, dtype=K.floatx())
 
         if self.x.ndim != 4:
             raise ValueError('Input data in `NumpyArrayIterator` '
